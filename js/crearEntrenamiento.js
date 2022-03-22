72fcc60e6f36aad4a267526d358080daefec2daf
Adrián <adrianncatalan@gmail.com>
2022-03-22 17:32:13 +0100

    Comenzando con la parte lógica de los ejercicios y y series y reps

--- js/crearEntrenamiento.js
+++ js/crearEntrenamiento.js
@@ -2,30 +2,31 @@ window.addEventListener('load', () => {
     let th = document.querySelectorAll('th');
     let tbody = document.querySelector('tbody');
     let bloque = document.getElementsByClassName('bloque');
-    let containerQuestion = document.getElementById('container-question');
+    let containerBloque = document.getElementById('containerBloque');
     var reiniciar = false;
 
     for (let i = 0; i < bloque.length; i++) {
         bloque[i].addEventListener('click', () => {
             let select = parseInt(bloque[i].innerHTML);
             if (select === i + 1) {
-                var estilos = window.getComputedStyle(containerQuestion);
+                var estilos = window.getComputedStyle(containerBloque);
                 var display = estilos.getPropertyValue('display');
                 if (display == 'flex') {
                     console.log('hola1')
-                    containerQuestion.style.setProperty('display', 'none');
+                    containerBloque.style.setProperty('display', 'none');
+                    document.getElementsByClassName('seleccionar')[0].style.setProperty('display', 'none');
                     var btn = document.createElement('button');
-                    btn.innerHTML = 'Reiniciar bloques de entrenamientos';
+                    btn.innerHTML = 'Reiniciar plan de entrenamiento';
                     btn.className = 'btn';
                     var containerBtn = document.createElement('div');
                     containerBtn.className = 'containerBtn';
                     containerBtn.append(btn);
-                    document.body.insertBefore(containerBtn, document.getElementById('containerTabla'));
+                    document.body.insertBefore(containerBtn, document.getElementById('main1'));
                     var containerBtn = document.getElementsByClassName('containerBtn');
                     containerBtn[0].addEventListener('click', () => {
                         console.log("hola2");
                         containerBtn[0].style.setProperty('display', 'none');
-                        containerQuestion.style.setProperty('display', 'flex');
+                        containerBloque.style.setProperty('display', 'flex');
                         reiniciar = true;
                         if (reiniciar) {
                             location.reload();
