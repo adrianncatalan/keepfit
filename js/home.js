9bcb81938b70fd8e80da1693a03151c4d90123df
JoanSebas <joan>
2022-04-17 20:01:48 +0200

    New Changes Home All

---
+++ js/home.js
@@ -0,0 +1,9 @@
+var dialog = document.querySelector('dialog');
+
+function openDialog(){
+    document.getElementById('dialogPerfil').show();
+}
+
+function closeDialog(){
+    document.getElementById('dialogPerfil').close();
+}
